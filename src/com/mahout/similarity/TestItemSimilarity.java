0b4fdc89d385ef8568bb1042030257189ddb5044
tianbx <tianbx@debain.com>
2014-02-28 18:04:42 +0800

    add a file of item-based recommendation

---
+++ src/com/mahout/similarity/TestItemSimilarity.java
@@ -0,0 +1,11 @@
+package com.mahout.similarity;
+
+import org.apache.mahout.cf.taste.impl.similarity.GenericItemSimilarity;
+
+public class TestItemSimilarity {
+
+	public static void main(String[] args) {
+//		GenericItemSimilarity
+	}
+
+}
